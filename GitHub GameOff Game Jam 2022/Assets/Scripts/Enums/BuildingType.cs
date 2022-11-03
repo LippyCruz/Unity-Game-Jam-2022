7353a7d0979d78d77d5daedfa3535f38a8d9e0b8
Gingaabread <none>
2022-11-03 19:07:47 +0100

    Added a UI queue

---
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/Enums/BuildingType.cs
@@ -0,0 +1,11 @@
+namespace BuildingManagement
+{
+    /// <summary>
+    /// Defines the different types of buildings that the player can 
+    /// build on their farm
+    /// </summary>
+    public enum BuildingType
+    {
+        ACRE
+    }
+}
